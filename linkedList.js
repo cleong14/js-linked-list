d30b3c255d1b25d211a08caf0aeea3efd245154f
Chaz Leong <cleong14@gmail.com>
2015-11-12 23:24:51 -1000

    completed all tests up to and including get method

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,73 @@
  */
 function linkedListGenerator(){
 
+  // create head, tail, and length for initial state
+  var head = null;
+  var tail = null;
+  var length = 0;
+
+  var newLinkedList = {
+
+    getHead: function () {
+      return head;
+    },
+
+    getTail: function () {
+      return tail;
+    },
+
+    add: function (value) {
+      var newNode;
+
+      if (!head) {
+
+        newNode = {
+          value: value,
+          next: null
+        };
+
+        head = newNode;
+        tail = newNode;
+
+      } else {
+
+        tail.next = {
+          value: value,
+          next: null
+        };
+
+        tail = tail.next;
+      }
+      length++;
+      return newNode;
+    },
+
+    remove: function () {
+
+    },
+
+    get: function (index) {
+      var currentNode = head;
+
+      if (!currentNode) {
+        return false;
+      } else {
+        // do some stuff
+        for (var i = 0; i < length; i++) {
+          if (index === i) {
+            return currentNode;
+          } else {
+            currentNode = currentNode.next;
+          }
+        }
+        return false;
+      }
+    },
+
+    insert: function () {
+
+    }
+  };
+
+  return newLinkedList;
 }
